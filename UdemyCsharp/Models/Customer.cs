2c58315ace68ec0443285c321cd7ff0bdad11016
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-01-12 12:02:26 +0530

    Data Annotations

--- UdemyCsharp/Models/Customer.cs
+++ UdemyCsharp/Models/Customer.cs
@@ -10,7 +10,7 @@ namespace UdemyCsharp.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please Enter your name")]//override default error message
         [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
