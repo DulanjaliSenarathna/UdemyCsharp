6ea6e63231758d897df15d3e210e9e1f09c0ebd2
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-01-12 17:58:59 +0530

    Refactoring Magic Numbers

--- UdemyCsharp/Models/MembershipType.cs
+++ UdemyCsharp/Models/MembershipType.cs
@@ -15,6 +15,10 @@ namespace UdemyCsharp.Models
         public short SignUpFee { get; set; }
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
-        
+
+        public static readonly byte Unknown = 0;
+        public static readonly byte PayAsYouGo = 1;
+
+
     }
 }
