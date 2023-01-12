307ddf593244e6af7dcd706341863cfd8c08d56f
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-01-12 10:32:29 +0530

    Exercise

--- UdemyCsharp/Controllers/MoviesController.cs
+++ UdemyCsharp/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
@@ -19,6 +20,16 @@ using UdemyCsharp.ViewModels;
         protected override void Dispose(bool disposing)
         {
             _context.Dispose();
+        }
+
+        public ViewResult New()
+        {
+            var genres = _context.Genres.ToList();
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+            return View("MovieForm",viewModel);
         }
 
         public ViewResult Index()
@@ -49,6 +60,43 @@ using UdemyCsharp.ViewModels;
                 Customers = customers
             };
             return View(viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+            return View("MovieForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+            }
+
+
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Movies");
+
         }
     }
 }
--- UdemyCsharp/Models/Movie.cs
+++ UdemyCsharp/Models/Movie.cs
@@ -14,14 +14,19 @@ namespace UdemyCsharp.Models
         [StringLength(255)]
         public string Name { get; set; }
 
-        [Required]
+        [Display(Name = "Genre")]
         public Genre Genre { get; set; }
+
+        [Required]
+        [Display(Name = "Genre")]
         public byte GenreId { get; set; }
 
         public DateTime DateAdded { get; set; }
 
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
 
+        [Display(Name = " Number In Stock")]
         public byte NumberInStock { get; set; }
     }
 }
