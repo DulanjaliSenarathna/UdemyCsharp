27100b1a714d89cf9d978cc03a4b87257185d0b3
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-01-13 22:52:20 +0530

    Using Camel Notation

--- UdemyCsharp/App_Start/WebApiConfig.cs
+++ UdemyCsharp/App_Start/WebApiConfig.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace UdemyCsharp
 {
@@ -9,6 +11,11 @@ namespace UdemyCsharp
     {
         public static void Register(HttpConfiguration config)
         {
+            //convert json key names into camel notation
+            var settings = config.Formatters.JsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Formatting.Indented;
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
