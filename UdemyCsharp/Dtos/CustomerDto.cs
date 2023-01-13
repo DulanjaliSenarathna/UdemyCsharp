2f484a0c9208130ccb3bbdcd72a29870ac25aaf7
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-01-13 21:54:44 +0530

    Data Transfer Objects

---
+++ UdemyCsharp/Dtos/CustomerDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using UdemyCsharp.Models;
+
+namespace UdemyCsharp.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please Enter your name")]//override default error message
+        [StringLength(255)]
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
+        public byte MembershipTypeId { get; set; } //implicitly required, because of byte data type
+
+        [Min18YearsIfAMember]
+        public DateTime? BirthDate { get; set; }
+    }
+}
