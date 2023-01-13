213cb315493a4aef8b5fc5a5e5feb602ca39c05b
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-01-13 22:40:52 +0530

    Auto Mapper

---
+++ UdemyCsharp/App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using UdemyCsharp.Dtos;
+using UdemyCsharp.Models;
+
+namespace UdemyCsharp.App_Start
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
--- UdemyCsharp/Global.asax.cs
+++ UdemyCsharp/Global.asax.cs
@@ -6,6 +6,8 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Http;
+using AutoMapper;
+using UdemyCsharp.App_Start;
 
 namespace UdemyCsharp
 {
@@ -13,6 +15,7 @@ namespace UdemyCsharp
     {
         protected void Application_Start()
         {
+            Mapper.Initialize(c=> c.AddProfile<MappingProfile>());
             GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
