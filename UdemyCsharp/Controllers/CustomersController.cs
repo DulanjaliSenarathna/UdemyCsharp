0922471a1e6e70f0f40d59c52a4c2ae763877792
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-01-15 08:20:23 +0530

    DataTables with Ajax Source

--- UdemyCsharp/Controllers/CustomersController.cs
+++ UdemyCsharp/Controllers/CustomersController.cs
@@ -76,8 +76,8 @@ namespace VidlyCSharp.Controllers
         // GET: Customer
         public ViewResult Index()
         {
-            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
-            return View(customers);
+            //var customers = _context.Customers.Include(c => c.MembershipType).ToList(); - remove because we fetching data through api
+            return View();
         }
 
         public ActionResult Details(int id)
