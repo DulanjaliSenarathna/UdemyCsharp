d8b246d294dfa9f1af3ac798ea0d4125c8dd0506
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-01-15 22:42:07 +0530

    Seeding Users and Roles

--- UdemyCsharp/Controllers/AccountController.cs
+++ UdemyCsharp/Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using UdemyCsharp.Models;
@@ -155,6 +156,12 @@ namespace UdemyCsharp.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    //Temp code
+                    //var roleStore = new RoleStore<IdentityRole>(new ApplicationDbContext());
+                    //var roleManager = new RoleManager<IdentityRole>(roleStore);
+                    //await roleManager.CreateAsync(new IdentityRole("CanManageMovie"));
+                    //await UserManager.AddToRoleAsync(user.Id, "CanManageMovie");
+
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
---
+++ UdemyCsharp/Migrations/202301151552294_SeedUsers.cs
@@ -0,0 +1,25 @@
+﻿namespace UdemyCsharp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SeedUsers : DbMigration
+    {
+        public override void Up()
+        {
+            Sql(@"
+                INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) VALUES (N'955dd967-3352-44aa-9360-58247ab33338', N'admin@vidly.com', 0, N'AHiAOioYYWlNmK28l0BNX/u6Uhk3IgyYj4+cz3u0S8bn9StoyrpNSwFtxs1VNzOTrw==', N'd79f0a82-bb39-4773-b2b5-c97a1f61e975', NULL, 0, 0, NULL, 1, 0, N'admin@vidly.com')
+                INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) VALUES (N'ec24ad56-abfc-49c5-823c-ce9bed26c0eb', N'guest@vidly.com', 0, N'AMlAIUkoB4Bt4ixu1UqOmXa6XUtleixxS8d6TnHK82vGxT41Hd7/xE67bsnuykEkJw==', N'db5dac9b-5d99-4c3f-8c58-79889c2f380f', NULL, 0, 0, NULL, 1, 0, N'guest@vidly.com')
+
+                INSERT INTO [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'03c13c67-ae3c-4933-a54e-0337f0118030', N'CanManageMovie')
+
+                INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'955dd967-3352-44aa-9360-58247ab33338', N'03c13c67-ae3c-4933-a54e-0337f0118030')
+
+            ");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
